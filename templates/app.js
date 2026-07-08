938c96b49ad79ce6c6619d9419f2f9e610ea4402
prodduturu likith kumar <kumarlikith178@gmail.com>
2026-07-08 12:40:54 +0530

    updated the app.js

--- templates/app.js
+++ templates/app.js
@@ -267,7 +267,7 @@ form.addEventListener('submit', async (e) => {
 
     let resultData;
     try {
-        const response = await fetch('http://localhost:8000/predict', {
+        const response = await fetch('https://mlops-based-delivery-delay-prediction.onrender.com/predict', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(payload)
